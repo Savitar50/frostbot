8b4d2bda6ad9ebc4b3f9bb64a8b6fce3d32f6238
ralphel <elisralph@gmail.com>
2017-12-11 07:34:11 -0500

    Updated purge number to 10000

--- bot.js
+++ bot.js
@@ -124,8 +124,8 @@ client.on("message", async message => {
     const deleteCount = parseInt(args[0], 10);
     
     // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    if(!deleteCount || deleteCount < 2 || deleteCount > 10000)
+      return message.reply("Please provide a number between 2 and 10000 for the number of messages to delete");
     
     // So we get our messages, and delete them. Simple enough, right?
     const fetched = await message.channel.fetchMessages({count: deleteCount});
